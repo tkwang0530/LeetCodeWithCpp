3f823a8c40c62d8ae8647c990d5377acea2cf701
Tommy <tkwang0530@gmail.com>
2024-06-30 22:18:19 +0800

    add LC1

---
+++ 0001/solution.hpp
@@ -0,0 +1,16 @@
+#ifndef SOLUTION_001_MAP
+#define SOLUTION_001_MAP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>&, int);
+};
+
+#endif
